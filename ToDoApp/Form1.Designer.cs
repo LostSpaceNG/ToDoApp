45b2c94752aacf80ea9a5dc7747c307fd714f8ea
Nikita <gantanikita@gmail.com>
2025-04-17 18:25:44 +0200

    Initial commit - ToDoApp MVP

---
+++ ToDoApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ToDoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTask = new TextBox();
+            btnAdd = new Button();
+            btnDelete = new Button();
+            clbTasks = new CheckedListBox();
+            SuspendLayout();
+            // 
+            // txtTask
+            // 
+            txtTask.Location = new Point(12, 12);
+            txtTask.Name = "txtTask";
+            txtTask.Size = new Size(329, 27);
+            txtTask.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            btnAdd.AutoSize = true;
+            btnAdd.Location = new Point(12, 56);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(128, 30);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "Add Task";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.AutoSize = true;
+            btnDelete.Location = new Point(12, 252);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(128, 30);
+            btnDelete.TabIndex = 3;
+            btnDelete.Text = "Delete Task";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // clbTasks
+            // 
+            clbTasks.FormattingEnabled = true;
+            clbTasks.Location = new Point(12, 122);
+            clbTasks.Name = "clbTasks";
+            clbTasks.Size = new Size(329, 114);
+            clbTasks.TabIndex = 5;
+            clbTasks.ItemCheck += clbTasks_ItemCheck;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(482, 553);
+            Controls.Add(clbTasks);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAdd);
+            Controls.Add(txtTask);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ToDo App";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtTask;
+        private Button btnAdd;
+        private Button btnDelete;
+        private CheckedListBox clbTasks;
+    }
+}
---
+++ ToDoApp/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Xml.Serialization;
+
+namespace ToDoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Connection String for DB Setup
+        string connectionString = "Data Source=tasks.db;Version=3;";
+
+        // Creates DB if doesn't exist
+        private void CreateDatabase()
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+
+                // Create Tasks Table with Id and Task Columns
+                string sql = @"CREATE TABLE IF NOT EXISTS Tasks (
+                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                Task TEXT,
+                                IsDone INTEGER DEFAULT 0
+                             )";
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Loads Tasks from DB
+        private void LoadTasks()
+        {
+            // create connection with DB
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                // create SQL command
+                string sql = "SELECT Task, IsDone FROM Tasks";
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+
+                // Read Data from column "Task" and add to Tasklist
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string task = reader["Task"].ToString()!;
+                        bool isDone = Convert.ToInt32(reader["IsDone"]) == 1;
+                        clbTasks.Items.Add(task, isDone);
+                    }
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Check the input field
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                // Save Task in DB
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "INSERT INTO Tasks (Task, IsDone) VALUES (@task, 0)";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@task", txtTask.Text);
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Add to Tasklist
+                clbTasks.Items.Add(txtTask.Text, false);
+                // Clear input field
+                txtTask.Clear();
+            }
+            else
+            {
+                // show Info Message
+                MessageBox.Show("Please enter a Task first.", "Task Empty", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Remove selected Task from Tasklist
+            if (clbTasks.SelectedIndex >= 0)
+            {
+                string taskText = clbTasks.SelectedItem!.ToString()!;
+
+                // Remove from DB
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "DELETE FROM Tasks WHERE Task = @task";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@task", taskText);
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Remove from UI
+                clbTasks.Items.RemoveAt(clbTasks.SelectedIndex);
+            }
+            else
+            {
+                // show Info Message
+                MessageBox.Show("Please select a Task first.", "Task Not Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void clbTasks_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            // Update the DB after the UI has updated
+            // (makes sure we are reading the correct, updated checkbox state)
+            this.BeginInvoke(new Action(() =>
+            {
+                string taskText = clbTasks.Items[e.Index].ToString()!;
+                int isDone = clbTasks.GetItemChecked(e.Index) ? 1 : 0;
+
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "UPDATE Tasks SET IsDone = @isDone WHERE Task = @task";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@isDone", isDone);
+                    cmd.Parameters.AddWithValue("@task", taskText);
+                    cmd.ExecuteNonQuery();
+                }
+            }));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateDatabase();
+            LoadTasks();
+        }
+    }
+}
